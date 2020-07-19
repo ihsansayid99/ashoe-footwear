8f2b2c7849a632d11c873c66a56bd53efc277e8d
Ihsan Sayid Muharram <61352696+ihsansayid99@users.noreply.github.com>
2020-07-20 00:09:17 +0700

    cart

---
+++ src/Pages/Cart.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import Title from '../components/Title';
+import { FaTrash } from 'react-icons/fa';
+
+export default class Cart extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            carts: JSON.parse(localStorage.getItem('products'))
+        }
+        this.cartItemDelete = this.cartItemDelete.bind(this);
+    }
+    cartItemDelete(_id) {
+        let getLocalStorage = JSON.parse(localStorage.getItem('products'))
+        let getStorages = getLocalStorage.map(getStorage => getStorage._id)
+        let index = getStorages.findIndex(id => id === _id);
+        this.state.carts.splice(index, 1);
+        const parsed = JSON.stringify(this.state.carts);
+        localStorage.setItem("products", parsed);
+        window.location.reload();
+    }
+    render() {
+        return (
+            <>
+                <Title title="My Cart" />
+                <div className="w-full py-16">
+                    <div className="text-center font-bold text-2xl">
+                        <h1>Keranjang Anda</h1>
+                    </div>
+                    <div className="py-8">
+                        {
+                            this.state.cart === null ?
+                                <h1 className="text-center">Keranjang Anda Kosong</h1>
+                                :
+                                <table className="table-auto mx-auto">
+                                    <thead className="text-left">
+                                        <tr>
+                                            <th className="px-4 py-2">Image</th>
+                                            <th className="px-4 py-2">Product</th>
+                                            <th className="px-4 py-2">Harga</th>
+                                            <th className="px-4 py-2">Quantity</th>
+                                            <th className="px-4 py-2">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="text-center">
+                                        {
+                                            this.state.carts.map((item, index) => (
+                                                <tr key={index}>
+                                                    <td className="border px-4 py-2"><img width="100" src={item.image} alt={item.name} /></td>
+                                                    <td className="border px-4 py-2">{item.name}</td>
+                                                    <td className="border px-4 py-2">{item.price}</td>
+                                                    <td className="border px-4 py-2">{item.quantity}</td>
+                                                    <td className="border px-4 py-2"><button onClick={(e, _id) => this.cartItemDelete(item._id)} className="px-4 py-2 border-2 border-red-700 hover:bg-black hover:border-black hover:text-white transition duration-150 ease-in rounded text-red-700"><FaTrash /></button></td>
+                                                </tr>
+                                            ))
+                                        }
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td className="border px-4 py-2">TOTAL :</td>
+                                            <td className="border px-4 py-2">{this.state.carts.reduce((res, item) => {
+                                                return res + (item.price * item.quantity)
+                                            }, 0)}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                        }
+                        <div className="text-center py-8">
+                            <button onClick={(e) => alert('Terimakasih... masih tahap progress')} className="w-64 py-3 mx-12 rounded bg-yellow-700 text-lg font-medium hover:bg-yellow-800 transition duration-150 ease-linear focus:outline-none">Check Out</button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
